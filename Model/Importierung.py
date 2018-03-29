fcd96c685c8083bdd4aa069370cd54effd1cab3d
Max Haslwanter <max.haslwanter@students.htlinn.ac.at>
2018-03-29 13:06:16 +0200

    "Aenderungen um das in Gruppen geteilte Ausgeben von Informationen, Fehlern und Lücken zu vereinfachen"

--- Model/Importierung.py
+++ Model/Importierung.py
@@ -1,15 +1,24 @@
 import csv
 
-
+info = []
+error = []
+gap = []
 def imp(address):
     file = open(address, "r")
     reader = csv.reader(file, delimiter=";")
     return reader
 
 
-def info(row, prev, wtr):
+def infos(row, prev):
     if row[0][0] == "#" and prev is not None and firstisdigit(prev):
-        wtr.writerow(prev[3] + "." + prev[2] + " " + prev[4] + ":" + prev[5] + ":" + prev[6] + "\n" + row[0] + "\n")
+        info.append(prev[3] + "." + prev[2] + " " + prev[4] + ":" + prev[5] + ":" + prev[6] + "\n" + row[0] + "\n")
+
+def writeInfos():
+    with open("info.csv", "w") as infos:
+        wtr = csv.writer(infos)
+        wtr.writerow("INFOS")
+        for row in info:
+            wtr.writerow(row)
 
 def delfirst(row, wtr):
     if firstisdigit(row):
@@ -19,14 +28,11 @@ def main(reader):
     prev = None
     with open("result.csv", "w") as result:
         wtr1 = csv.writer(result)
-        with open("info.csv", "w") as info:
-            wtr2 = csv.writer(info)
-            wtr2.writerow("INFOS")
-            for row in reader:
-                info(row, prev, wtr2)
-                delfirst(row, wtr1)
-                findgap(row, prev)
-                prev = row
+        for row in reader:
+            infos(row, prev)
+            delfirst(row, wtr1)
+            prev = row
+    writeInfos()
 
 def firstisdigit(row):
     if row[0][1].isdigit() and row[0][2].isdigit() and row[0][0] == " ":
